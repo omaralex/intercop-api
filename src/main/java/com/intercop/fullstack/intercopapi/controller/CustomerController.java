639edcd4100830133e96752519d21d8fb66fadf6
Omar Muniz <omar.muniz@LIM-WS1831.local>
2020-11-22 14:55:38 -0500

    Adding cors

--- src/main/java/com/intercop/fullstack/intercopapi/controller/CustomerController.java
+++ src/main/java/com/intercop/fullstack/intercopapi/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class CustomerController {
 
